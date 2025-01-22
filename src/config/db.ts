65f1f48a581123017b6792aaf5ace92e0553ffb2
benjasx <benjasanchez175@gmail.com>
2025-01-21 20:46:22 -0700

    se crea la conexion mediante las variables de entorno

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,4 +1,7 @@
 import { Sequelize } from "sequelize";
+import dotev from 'dotenv';
 
-const db = new Sequelize('postgresql://rest_api_products_pc9j_user:LX6LKkVsY6TgOGNcJZKZxhFz1vpe5AH0@dpg-cu868ktds78s73a44k2g-a.oregon-postgres.render.com/rest_api_products_pc9j?ssl=true')
+dotev.config()
+
+const db = new Sequelize(process.env.DATABASE_URL!)
 export default db
