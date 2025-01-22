703afc756e059524c471a91a1a63458882b932fa
benjasx <benjasanchez175@gmail.com>
2025-01-21 20:13:24 -0700

    Se agregan las peticiones http y router

---
+++ src/router.ts
@@ -0,0 +1,26 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get("/", (req, res) => {
+  res.json("Desde GET");
+});
+
+router.post("/", (req, res) => {
+  res.json("Desde POST");
+});
+
+router.patch("/", (req, res) => {
+  res.json("Desde PATCH");
+});
+
+router.put("/", (req, res) => {
+  res.json("Desde PUT");
+});
+
+router.delete("/", (req, res) => {
+  res.json("Desde DELETE");
+});
+
+
+export default router
--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,8 @@
 import express from "express";
+import router from "./router";
 
 const server = express();
 
-//ROUTING
-server.get("/", (req, res) => {
-  res.json("Desde GET");
-});
-
-server.post("/", (req, res) => {
-  res.json("Desde POST");
-});
-
-server.patch("/", (req, res) => {
-  res.json("Desde PATCH");
-});
-
-server.put("/", (req, res) => {
-    res.json("Desde PUT");
-  });
-
-server.delete("/", (req, res) => {
-  res.json("Desde DELETE");
-});
+server.use('/api/products', router)
 
 export default server;
