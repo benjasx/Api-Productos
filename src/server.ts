6e0ef9078e6ab600b16eb5ff957de4ab9235c891
benjasx <benjasanchez175@gmail.com>
2025-01-21 20:06:44 -0700

    Se

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,26 @@
-import  express  from "express";
+import express from "express";
 
-const server = express()
+const server = express();
 
 //ROUTING
-server.get('/',(req,res) =>{
-    const datos = [
-        {id:1, nombre:'Benja'},
-        {id:2, nombre:'Hermi'}
-    ]
-    res.json(datos)
-})
+server.get("/", (req, res) => {
+  res.json("Desde GET");
+});
 
-export default server
+server.post("/", (req, res) => {
+  res.json("Desde POST");
+});
+
+server.patch("/", (req, res) => {
+  res.json("Desde PATCH");
+});
+
+server.put("/", (req, res) => {
+    res.json("Desde PUT");
+  });
+
+server.delete("/", (req, res) => {
+  res.json("Desde DELETE");
+});
+
+export default server;
