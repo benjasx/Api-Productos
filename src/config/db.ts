d7bdb66be75fa96ce25087c8897b0bdee0ae928a
benjasx <benjasanchez175@gmail.com>
2025-01-22 20:32:30 -0700

    se crea el model de la tabla

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,7 +1,9 @@
-import { Sequelize } from "sequelize";
+import { Sequelize } from "sequelize-typescript";
 import dotev from 'dotenv';
 
 dotev.config()
 
-const db = new Sequelize(process.env.DATABASE_URL!)
+const db = new Sequelize(process.env.DATABASE_URL!,{
+    models:[__dirname + '/../models/**/*.ts']
+})
 export default db
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import router from "./router";
 import db from "./config/db";
+import colors from 'colors'
 
 
 //CONECTAR A LA BBDD
@@ -9,10 +10,10 @@ async function connectDB() {
   try {
     await db.authenticate()
     db.sync()
-    console.log('Conexion exitosa a la Base de Datos')
+    console.log(colors.bgBlue.white('Conexion exitosa a la Base de Datos'))
   } catch (error) {
     console.log(error)
-    console.log('Hubo un error al intertar conectar a la base de datos')
+    console.log(colors.bgRed.white('Hubo un error al conectar a BD'))
   }
 }
 connectDB()
