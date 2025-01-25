632a67f1a21d226ab3391ffbe880cc7f27589e8b
benjasx <benjasanchez175@gmail.com>
2025-01-24 20:56:35 -0700

    ACTUALIZAR CON PUT

--- src/handlers/productos.ts
+++ src/handlers/productos.ts
@@ -1,44 +1,70 @@
-import { Request,Response } from "express";
+import { Request, Response } from "express";
 import Products from "../models/Products.model";
 
-
-export const getProducts = async (req:Request, res:Response) => {
- try {
-  const products = await Products.findAll({
-    order:[
-      ['id', 'DESC']
-    ],
-    attributes: {exclude: ['updatedAt', 'createdAt', 'availability']}
-    //limit:2
-  })
-  res.json({data: products})
- } catch (error) {
-  console.log(error)
- }
-}
-
-
-export const getProductByID = async (req:Request, res:Response) => {
+export const getProducts = async (req: Request, res: Response) => {
   try {
-    const {id} = req.params
-    const product = await Products.findByPk(id)
-    if(!product){
+    const products = await Products.findAll({
+      order: [["id", "DESC"]],
+      attributes: { exclude: ["updatedAt", "createdAt", "availability"] },
+      //limit:2
+    });
+    res.json({ data: products });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getProductByID = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const product = await Products.findByPk(id);
+    if (!product) {
       return res.status(404).json({
-        error: 'producto no encontrado'
-      })
+        error: "producto no encontrado",
+      });
     }
-    res.json({data:product})
+    res.json({ data: product });
   } catch (error) {
-   console.log(error)
+    console.log(error);
   }
- }
+};
 
-export const createProducts = async (req:Request, res:Response) => {
-
+export const createProducts = async (req: Request, res: Response) => {
   try {
-    const product =  await  Products.create(req.body)
-    res.json({data: product});
+    const product = await Products.create(req.body);
+    res.json({ data: product });
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
+};
+
+export const updateProduct = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const product = await Products.findByPk(id);
+  if (!product) {
+    return res.status(404).json({
+      error: "producto no encontrado",
+    });
+  }
+
+  //ACTUALIZAR PRODUCTO
+  await product.update(req.body)
+  await product.save()
+  res.json({data:product})
+};
+
+
+export const updateAvailability = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const product = await Products.findByPk(id);
+  if (!product) {
+    return res.status(404).json({
+      error: "producto no encontrado",
+    });
+  }
+
+  //ACTUALIZAR PRODUCTO
+  await product.update(req.body)
+  await product.save()
+  res.json({data:product})
 };
--- src/router.ts
+++ src/router.ts
@@ -1,40 +1,64 @@
 import { Router } from "express";
-import {body, param} from 'express-validator'
-import {createProducts, getProductByID, getProducts} from "./handlers/productos";
+import { body, param } from "express-validator";
+import {
+  createProducts,
+  getProductByID,
+  getProducts,
+  updateAvailability,
+  updateProduct,
+} from "./handlers/productos";
 import { handleInputErrors } from "./middleware";
-
 
 const router = Router();
 
-router.get("/", getProducts)
-router.get('/:id', 
-  param('id').isInt().withMessage('ID no válido'),
+router.get("/", getProducts);
+router.get(
+  "/:id",
+  param("id").isInt().withMessage("ID no válido"),
   handleInputErrors,
   getProductByID
-)
+);
 
-router.post("/", 
+router.post(
+  "/",
   //Validation
- body('name')
-  .notEmpty().withMessage('El nombre de producto no debe estar vacio'),
- body('price')
-  .isNumeric().withMessage('Valor no valido')
-  .notEmpty().withMessage('El precio de producto no debe estar vacio')
-  .custom(value => value > 0).withMessage('El precio debe ser mayor a 0'),
+  body("name")
+    .notEmpty()
+    .withMessage("El nombre de producto no debe estar vacio"),
+  body("price")
+    .isNumeric()
+    .withMessage("Valor no valido")
+    .notEmpty()
+    .withMessage("El precio de producto no debe estar vacio")
+    .custom((value) => value > 0)
+    .withMessage("El precio debe ser mayor a 0"),
   handleInputErrors,
-  createProducts );
+  createProducts
+);
 
-router.patch("/", (req, res) => {
-  res.json("Desde PATCH");
-});
+router.patch("/:id", updateAvailability);
 
-router.put("/", (req, res) => {
-  res.json("Desde PUT");
-});
+router.put(
+  "/:id",
+  //Validation
+  body("name")
+    .notEmpty()
+    .withMessage("El nombre de producto no debe estar vacio"),
+  body("price")
+    .isNumeric()
+    .withMessage("Valor no valido")
+    .notEmpty()
+    .withMessage("El precio de producto no debe estar vacio")
+    .custom((value) => value > 0)
+    .withMessage("El precio debe ser mayor a 0"),
+  body("availability")
+  .isBoolean().withMessage('Valor para dispinibilidad no valido'),
+  handleInputErrors,
+  updateProduct
+);
 
 router.delete("/", (req, res) => {
   res.json("Desde DELETE");
 });
 
-
-export default router
+export default router;
