2be90326440479bdd0fe0da45c5a8e2df175be6a
benjasx <benjasanchez175@gmail.com>
2025-01-30 20:18:14 -0700

    Pruebas para api/products -- validar que no admita valores vacios

--- src/handlers/__test__/product.test.ts
+++ src/handlers/__test__/product.test.ts
@@ -4,6 +4,20 @@ import server from "../../server";
 
 
 describe('POST /api/products', () => {
+    
+    it('should display validation errors', async() => {
+        const response = await request(server).post('/api/products').send({
+            
+        })
+
+        expect(response.status).toBe(400)
+        expect(response.body).toHaveProperty('errors')
+        expect(response.body.errors).toHaveLength(4)
+
+        expect(response.status).not.toBe(404)
+        expect(response.body.errors).not.toHaveLength(2)
+    })
+    
     it('Should create new product', async() =>{
         const response = await request(server).post('/api/products').send({
             name: "Mascara Gamer Azul",
