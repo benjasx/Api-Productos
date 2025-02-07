648f4b6e6235b5af6d6605c489ca13576a821a88
benjasx <benjasanchez175@gmail.com>
2025-02-06 20:23:13 -0700

    Personalizando swagger colors and icons

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -1,4 +1,5 @@
 import swaggerJSDoc from "swagger-jsdoc";
+import { SwaggerUiOptions } from "swagger-ui-express";
 
 
 const options : swaggerJSDoc.Options =  {
@@ -13,7 +14,7 @@ const options : swaggerJSDoc.Options =  {
         info:{
             title:'REST API node.js / Express / TypeScript',
             version:'1.0.0',
-            description:'API Docs for Products'
+            description:'API Docs for Products by: Ing. Benjamín S.'
         }
     },
     apis:['./src/router.ts'],
@@ -21,4 +22,21 @@ const options : swaggerJSDoc.Options =  {
 
 
 const swaggerSpec = swaggerJSDoc(options)
+
+const swaggerUiOptions : SwaggerUiOptions = {
+    customCss:`
+        .topbar-wrapper .link{
+            content: url('https://imgs.search.brave.com/wejBI3K-vnAG_I1Cu_QAxDYT6dp7nOsxA4sP_Wjlljc/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93YWxs/cGFwZXJzLmNvbS9p/bWFnZXMvaGQvcmlj/a2FuZC1tb3J0eS1w/b3J0YWwtZXNjYXBl/LXN0aWNrZXItdThm/ZWw5dzB4d281bXY2/Mi5wbmc');
+            height: 30px:
+            width: auto;
+        }
+        .swagger-ui .topbar{
+            background-color: #000;
+        }
+    `,
+    customSiteTitle: 'La documentación de mi API'
+}
 export default swaggerSpec
+export{
+    swaggerUiOptions
+}
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import colors from 'colors'
 import swaggerUI from 'swagger-ui-express'
-import swaggerSpec from "./config/swagger";
+import swaggerSpec, {swaggerUiOptions} from "./config/swagger";
 import router from "./router";
 import db from "./config/db";
 
@@ -29,5 +29,5 @@ server.use('/api/products', router)
 
 
 //DOCs
-server.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec))
+server.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec,swaggerUiOptions))
 export default server;
